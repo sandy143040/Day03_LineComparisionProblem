e9c97d64f0b3bae4a9c96b9bc24c61f49c5f6bf8
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-06-18 04:35:13 +0530

    [Sandeep] Add: Length of line

---
+++ Line_Comparision/Line_Comparision/Line.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision
+{
+    public class Line
+    {
+        private double x1;
+        private double y1;
+        private double x2;
+        private double y2;
+
+        public Line(double x1, double y1, double x2, double y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public double CalculateLength()
+        {
+            double deltaX = x2 - x1;
+            double deltaY = y2 - y1;
+            double length = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+            return length;
+        }
+    }
+}
---
+++ Line_Comparision/Line_Comparision/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the coordinates of the first point (x1, y1):");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the coordinates of the second point (x2, y2):");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            Line line = new Line(x1, y1, x2, y2);
+            double length = line.CalculateLength();
+            Console.WriteLine("The length of the line is: " + length);
+
+            Console.ReadLine();
+        }
+    }
+}

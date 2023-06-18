17a9a7221b99a0643d4a4adab904e4024dde7b9e
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-06-18 17:10:42 +0530

    [Sandeep] Add: Comparing Line1 & Line2 are equals or not

--- Line_Comparision/Line_Comparision/Line.cs
+++ Line_Comparision/Line_Comparision/Line.cs
@@ -12,21 +12,43 @@ namespace Line_Comparision
         private double y1;
         private double x2;
         private double y2;
+        private double a1;
+        private double b1;
+        private double a2;
+        private double b2;
 
-        public Line(double x1, double y1, double x2, double y2)
+        public Line(double x1, double y1, double x2, double y2, double a1, double b1, double a2, double b2)
         {
             this.x1 = x1;
             this.y1 = y1;
             this.x2 = x2;
             this.y2 = y2;
+            this.a1 = a1;
+            this.b1 = b1;
+            this.a2 = a2;
+            this.b2 = b2;
         }
 
-        public double CalculateLength()
+        public void CalculateLength()
         {
             double deltaX = x2 - x1;
             double deltaY = y2 - y1;
-            double length = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
-            return length;
+            double deltaA = a2 - a1;
+            double deltaB = b2 - b1;
+            double lengthOfLine1 = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+            Console.WriteLine("Length of Line1 is: "+ lengthOfLine1);
+            double lengthOfLine2 = Math.Sqrt(Math.Pow(deltaA, 2) + Math.Pow(deltaB, 2));
+            Console.WriteLine("Length of Line2 is: "+ lengthOfLine2);
+
+            //Check equality of two lines
+            if (lengthOfLine1.Equals(lengthOfLine2))
+            {
+                Console.WriteLine("length of line1 and line2 are equals");
+            }
+            else
+            {
+                Console.WriteLine("length of line1 and line2 are not equal equals");
+            }
         }
     }
 }
--- Line_Comparision/Line_Comparision/Program.cs
+++ Line_Comparision/Line_Comparision/Program.cs
@@ -10,17 +10,26 @@ namespace Line_Comparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the coordinates of the first point (x1, y1):");
+            Console.WriteLine("Enter the coordinates of the first point of Line1 (x1, y1):");
             double x1 = Convert.ToDouble(Console.ReadLine());
             double y1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter the coordinates of the second point (x2, y2):");
+            Console.WriteLine("Enter the coordinates of the second point of Line1 (x2, y2):");
             double x2 = Convert.ToDouble(Console.ReadLine());
             double y2 = Convert.ToDouble(Console.ReadLine());
 
-            Line line = new Line(x1, y1, x2, y2);
-            double length = line.CalculateLength();
-            Console.WriteLine("The length of the line is: " + length);
+            Console.WriteLine("Enter the coordinates of the first point of Line2 (a1, b1):");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the coordinates of the first point of Line2 (a2, b2):");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+
+            Line line = new Line(x1, y1, x2, y2, a1, b1, a2, b2);
+            //double length = Convert.ToDouble(line.CalculateLength());
+            line.CalculateLength();
+            //Console.WriteLine("The length of the line is: " + length);
 
             Console.ReadLine();
         }
